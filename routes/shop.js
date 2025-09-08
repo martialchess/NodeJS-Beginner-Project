c693fe4adea1240db2a35853467acd5735a64e9d
Rida Malik <internalprogression@gmail.com>
2025-09-08 10:00:29 +0500

    feat: add product id to path and fix related typos

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
-route.get('/orders', shopController.getOrders)
+router.get('/orders', shopController.getOrders)
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
