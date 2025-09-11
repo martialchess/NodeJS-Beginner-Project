f270df88eceb034016794e356d555a056c25726f
Rida Malik <internalprogression@gmail.com>
2025-09-12 04:56:37 +0500

    Implement product editing (query params, pre-populate edit form, edit product data) and clean up views.

--- models/product.js
+++ models/product.js
@@ -2,23 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-      path.dirname(require.main.filename),
-      'data',
-      'products.json'
-    );
-    
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
 const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
       cb(JSON.parse(fileContent));
-      }
-    });
-}
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +27,24 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err =>{
-            console.log(err);
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
         });
-    });      
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
   }
 
   static fetchAll(cb) {
@@ -44,8 +56,5 @@ module.exports = class Product {
       const product = products.find(p => p.id === id);
       cb(product);
     });
-  }  
+  }
 };
-
-
-
