072841296d02fc6d8c3b5fe8e64565d122919647
Rida Malik <internalprogression@gmail.com>
2025-09-12 07:42:27 +0500

    Merge Module 9: Dynamic routes, cart functionality, and product delete

--- routes/admin.js
+++ routes/admin.js
@@ -18,7 +18,10 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/edit-product => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+// /admin/edit-product => POST
 router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
 
