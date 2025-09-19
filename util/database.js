589570105d334820f6a8bcf2000d8c721bb47c0a
Rida Malik <internalprogression@gmail.com>
2025-09-19 13:39:09 +0500

    feat(mongodb): setup MongoDB driver, establish and test database connection.

--- util/database.js
+++ util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_complete', 'root', 'Westworld123/', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://rida_main:DKFQyJT3cSYASrxV@cluster0.cxbajmp.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+    )
+        .then(client => {
+            console.log('Connected');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+    };
+
+    const getDb = () => {
+        if (_db) {
+            return _db;
+        }
+        throw 'No database found.';
+    };
+
+    // module.exports = mongoConnect;
+    exports.mongoConnect = mongoConnect;
+    exports.getDb = getDb;
+
