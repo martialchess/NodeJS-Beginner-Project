39eec114ed6476c42feace7a29d31ce4945dfba2
Rida Malik <internalprogression@gmail.com>
2025-09-21 14:19:01 +0500

    feat(products): implement update & delete operations, fix add-product logic

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin', adminRoutes);
+// app.use('/admin/products', adminRoutes)
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- models/product.js
+++ models/product.js
@@ -3,18 +3,28 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
       .collection('products')
-      .insertOne(this)
+      .updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db
+      .collection('products')
+      .insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -47,6 +57,18 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
       })
       .catch(err => {
         console.log(err);
