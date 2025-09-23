7c7ced6d78357d812eea6aa790587e517fad91bb
Rida Malik <internalprogression@gmail.com>
2025-09-23 12:13:55 +0500

    feat(cart): implement cart model with add, display, and delete functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('68d13cefb064feb2db6a0960')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
