23b4c2b980f18d972d8220bfc0f596eeb46ce19c
Rida Malik <internalprogression@gmail.com>
2025-10-16 06:57:48 +0500

    feat: connect sessions to MongoDB store.

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,8 +25,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false})
-);
+app.use(session({
+  secret: 'my secret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: store
+}));
 
 app.use((req, res, next) => {
   User.findById('68ee0eaf09c1b264f50bda62')
