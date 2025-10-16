32219cd76d36dd421f5381230d4ececf777dee4e
Rida Malik <internalprogression@gmail.com>
2025-10-16 06:07:06 +0500

    feat: add and configure session management.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -19,6 +20,8 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false})
+);
 
 app.use((req, res, next) => {
   User.findById('68ee0eaf09c1b264f50bda62')
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,19 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[4]
-    .trim()
-    .split('=')[1] === 'true';
+    // const isLoggedIn = req
+    // .get('Cookie')
+    // .split(';')[4]
+    // .trim()
+    // .split('=')[1] === 'true';
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
