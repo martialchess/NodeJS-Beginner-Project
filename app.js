720c593600bca958d73f442749c91ca4cc7dcd7a
Rida Malik <internalprogression@gmail.com>
2025-10-16 14:45:03 +0500

    fix: make add-to-cart work again.

--- app.js
+++ app.js
@@ -32,14 +32,17 @@ app.use(session({
   store: store
 }));
 
-// app.use((req, res, next) => {
-//   User.findById('68ee0eaf09c1b264f50bda62')
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
