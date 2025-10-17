fbd79b33e53cafb77dd5154ffd4d4a37ec37d4f9
Rida Malik <internalprogression@gmail.com>
2025-10-17 06:33:38 +0500

    feat: add password encryption with bcrypt.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -17,14 +19,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('68ee0eaf09c1b264f50bda62')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+      .compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          });
+        }
+        res.redirect('/login');
+      })
+      .catch(err => {
         console.log(err);
-        res.redirect('/');
-      });
+        res.redirect('/login');
+      }); 
     })
     .catch(err => console.log(err));
 };
@@ -33,24 +51,28 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email})
-  .then(userDoc => {
-    if (userDoc) {
-      return res.redirect('/signup');
-    }
-    const user = new User({
-      email: email,
-      password: password,
-      cart: { items: [] }
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
+    })
+    .catch(err => {
+      console.log(err);
     });
-    return user.save();
-})
-  .then(result => {
-    res.redirect('/login');
-  })
-  .catch(err => {
-    console.log(err);
-  });
 };
 
 exports.postLogout = (req, res, next) => {
@@ -59,6 +81,7 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
 
 
 
