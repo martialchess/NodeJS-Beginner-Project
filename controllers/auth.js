8a44b3428fe2b628c2dbb6e39797db8375407f67
Rida Malik <internalprogression@gmail.com>
2025-10-18 07:24:15 +0500

    feat: added email func with sendGrid and Nodemailer integration.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: process.env.SENDGRID_API_KEY
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -83,7 +91,16 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
-        });
+          return transporter.sendMail({
+            to: email,
+            from: 'editor@yourdomain.com',
+            subject: 'Signup Successful',
+            html: '<h1>Congratulations! You have successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
+      });
     })
     .catch(err => {
       console.log(err);
