a9cd696ec903472a544036c3ea08773ec42ac3c0
Rida Malik <internalprogression@gmail.com>
2025-10-19 13:53:23 +0500

    feat: implement basic error throwing and error page rendering.

--- app.js
+++ app.js
@@ -46,10 +46,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -62,7 +67,10 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404); 
+
 
 mongoose
   .connect(process.env.MONGODB_URI)
