11a211b3b3dd689d054e032ff806867280fc4cd6
Rida Malik <internalprogression@gmail.com>
2025-10-20 14:50:19 +0500

    feat: add centralized error handling and status code improvements.

--- controllers/auth.js
+++ controllers/auth.js
@@ -112,7 +112,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -156,8 +160,10 @@ exports.postSignup = (req, res, next) => {
           });
         })
         .catch(err => {
-          console.log(err);
-      });
+          const error = new Error(err);
+          error.httpStatusCode = 500;
+          return next(error);
+        });
 };
 
 exports.postLogout = (req, res, next) => {
@@ -224,7 +230,9 @@ exports.postReset = (req, res, next) => {
         });
       })
       .catch(err => {
-        console.log(err);
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
       });
   });
 };
@@ -248,7 +256,9 @@ exports.getNewPassword = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -277,6 +287,8 @@ exports.postNewPassword = (req, res, next) => {
       res.redirect('/login');
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
